bd8da33ac7d071990e84aebde04f63f5e9d8aa04
Justin Mitchel <jmitchel3@users.noreply.github.com>
2025-04-22 15:17:21 -0600

    9 - Extract YouTube ID & Redirect

--- src/components/YouTubeUrlForm.jsx
+++ src/components/YouTubeUrlForm.jsx
@@ -1,22 +1,59 @@
+"use client"
+
+import extractYouTubeInfo from "@/lib/extractYouTubeInfo"
+import { redirect } from "next/navigation"
+import { useState } from "react"
+
+
+
 export default function YouTubeUrlForm() {
-  return (
-    <div>
-      <label htmlFor="price" className="block text-sm/6 font-medium text-gray-900">
-        YouTube Video
-      </label>
-      <div className="mt-2">
-        <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
-          <div className="shrink-0 text-base text-gray-500 select-none sm:text-sm/6">URL</div>
-          <input
-            id="url"
-            name="url"
-            type="text"
-            placeholder="https://www.youtube.com/watch?v=..."
-            className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-          />
-          
+    const [url, setUrl] = useState('')
+    const [videoData, setVideoData] = useState({
+        videoId: '',
+        time: '',
+    })
+    const handleSubmit = event => {
+        event.preventDefault()
+        if (!videoData.videoId) {
+            alert("Needs a video id")
+        } else {
+            redirect(`/watch?v=${videoData.videoId}&t=${videoData.time}`)
+        }
+       
+    }
+    const handleUrlChange = event => {
+        let changedUrl = event.target.value ? event.target.value : ''
+        setUrl(changedUrl)
+        let {videoId, time} = extractYouTubeInfo(changedUrl)
+        setVideoData({videoId: videoId, time: time ? time : 0})
+    }
+    
+    return (
+        <form onSubmit={handleSubmit}>
+      <div className="w-full">
+        <div className="relative w-full">
+          <div className="flex items-center w-full rounded-full border border-gray-200 hover:shadow-md focus-within:shadow-md transition-shadow duration-200">
+            <input
+              id="url"
+              name="url"
+              onChange={handleUrlChange}
+              value={url}
+              type="text"
+              required
+              placeholder="Enter YouTube URL"
+              className="block w-full py-4 px-6 text-lg text-gray-900 placeholder:text-gray-400 focus:outline-none rounded-full"
+            />
+            <button 
+                type='submit'
+                className="absolute right-2 bg-black hover:bg-red-600 text-white font-medium py-2 px-6 rounded-full transition-colors duration-200">
+              Play
+            </button>
+          </div>
+          <div>
+            {videoData && JSON.stringify(videoData)}
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
+      </form>
+    )
+  }
